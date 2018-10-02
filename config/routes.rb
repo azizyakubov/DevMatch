4b76d5d4eb2e2f1f673857065e2d1f2130ea8b84
Aziz Yakubov <azizy15@gmail.com>
2018-10-01 17:26:53 -0700

    added serverside code for creating subscriptions for pro users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
 
-  get 'about', to: 'pages#about'
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 
+  get 'about', to: 'pages#about'
   resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new'
 end
